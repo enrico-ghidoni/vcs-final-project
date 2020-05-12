d215eddde3aee243af7b374b8a3111c7a982aca1
Enrico Ghidoni <enricoghdn@gmail.com>
2020-05-12 16:05:25 +0200

    Aggiunta parte di detection

--- progetto/detection.py
+++ progetto/detection.py
@@ -9,10 +9,10 @@ parser.add_argument('--video',
                     help='Absolute path for video file to process.')
 parser.add_argument('--output-file',
                     help='Absolute path for file containing regions detected as paintings.')
-parser.add_argument('--skip',
-                    help='Number of frames to skip after every processed frame.',
+parser.add_argument('--onein',
+                    help='Evaluate one frame every N frames.',
                     type=int,
-                    default=0)
+                    default=1)
 parser.add_argument('--debug',
                     help='Enable debug logging',
                     action='store_true')
@@ -23,43 +23,59 @@ def close(event):
         plt.close(event.canvas.figure)
 
 
-def main(video, output_file, skip, debug=False):
+def main(video, output_file, onein, debug=False):
+    frame_count = 1
+
     print(f'main starting with video {video}')
     cap = cv2.VideoCapture(video)
-    print(cap.isOpened())
     while(cap.isOpened()):
         ret, frame = cap.read()
+        if frame_count % onein == 0:
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        filtered = cv2.GaussianBlur(gray, (5, 5), 0)
-        otsu_th, th_img = cv2.threshold(filtered, 0, 127, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            std = np.std(gray)
+            gf_size = int(np.ceil(3 * std) // 2 * 2 + 1)
+            gk_size = (2 * gf_size + 1, 2 * gf_size + 1)
 
-        edges = cv2.Canny(gray, .8 * otsu_th, otsu_th)
+            filtered = cv2.GaussianBlur(gray, gk_size, 0)
+            otsu_th, th_img = cv2.threshold(filtered, 0, 127, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        fig, ax = plt.subplots(2, 2, figsize=(24, 14))
+            edges = cv2.Canny(gray, .8 * otsu_th, otsu_th)
 
-        ax[0, 0].imshow(gray, cmap='gray')
-        ax[0, 0].set_title('grayscale image')
-        ax[0, 1].imshow(th_img, cmap='gray')
-        ax[0, 1].set_title('after otsu thresholding')
-        ax[1, 0].imshow(edges, cmap='gray')
-        ax[1, 0].set_title('edges with canny')
+            contours_img = np.copy(rgb_frame)
+            contours, hierarchy = cv2.findContours(th_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(contours_img, contours, -1, (0, 255, 0), 3)
 
-        # plt.subplot(221)
-        # plt.imshow(gray, cmap='gray', aspect='auto')
-        # plt.subplot(222)
-        # plt.imshow(th_img, cmap='gray', aspect='auto')
-        # plt.subplot(223)
-        # plt.imshow(edges, cmap='gray', aspect='auto')
-        plt.gcf().canvas.mpl_connect('key_press_event', close)
-        plt.show()
+            fig, ax = plt.subplots(2, 2, figsize=(24, 14))
 
-        # key = cv2.waitKey(0)
-        # while key not in [ord('q'), ord('k')]:
-        #     key = cv2.waitKey(0)
-        # if key == ord('q'):
-        #     break
+            fig.canvas.set_window_title(f'Frame {frame_count}')
+
+            ax[0, 0].imshow(rgb_frame)
+            ax[0, 0].set_title('original frame')
+            ax[0, 1].imshow(th_img, cmap='gray')
+            ax[0, 1].set_title('after otsu thresholding')
+            ax[1, 0].imshow(filtered, cmap='gray')
+            ax[1, 0].set_title('after gaussian filter')
+            ax[1, 1].imshow(contours_img)
+            ax[1, 1].set_title('contours')
+
+            # plt.subplot(221)
+            # plt.imshow(gray, cmap='gray', aspect='auto')
+            # plt.subplot(222)
+            # plt.imshow(th_img, cmap='gray', aspect='auto')
+            # plt.subplot(223)
+            # plt.imshow(edges, cmap='gray', aspect='auto')
+            plt.gcf().canvas.mpl_connect('key_press_event', close)
+            plt.show()
+
+            # key = cv2.waitKey(0)
+            # while key not in [ord('q'), ord('k')]:
+            #     key = cv2.waitKey(0)
+            # if key == ord('q'):
+            #     break
+        frame_count += 1
             
     cap.release()
     cv2.destroyAllWindows()
