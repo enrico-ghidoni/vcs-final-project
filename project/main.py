d385b337b56ca84c665390033866b294f3b89903
Simone Ferrari <44051210+sgherro@users.noreply.github.com>
2020-05-16 11:50:00 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -5,26 +5,28 @@ from rectification import *
 
 #images loading 
 #TODO: implement the image loading by json files
-image_original = cv2.imread(r"C:\Users\simof\Pictures\Saved Pictures\gallery_2.jpg")
-#image_db = image.imread(r"C:\Users\simof\Pictures\Saved Pictures\gallery_db1.jpg")
+image_original = cv2.imread(r"Image you want")
     
 print("\n RECTIFICATION \n Rectification image process starting .....")
 
 images = [image_original]
 img_p = perspective_correction(perspective_correction(image_original))
 images.append(img_p)
-#image_original, cont = rectification_db(img_p,image_db)
-#images.append(image_db)
-images.append(img_p)
 
-#show a sequence of images
+#considering an image matching from the paintings DB and the corrisponded rectified image
+# from previous step
+image_db = image.imread(r"Corrisponent image from DB")
+image_original, cont = rectification_db(img_p,image_db)
+images.append(image_db)
+images.append(cont)
+
+#show all the images
 for i in images:
     cv2.imshow("",i)
 cv2.waitKey(0)
 
 #process to save a common image and the corresponded rectified image
-name = input("Write image name:")
-print("Original image saved: ", cv2.imwrite(r"C:\Users\simof\Pictures\Saved Pictures\Rec_Image\Video Images\i{0}.png".format(name),image_original))
-print("Rectified image saved: ", cv2.imwrite(r"C:\Users\simof\Pictures\Saved Pictures\Rec_Image\Rectified\i{0}.png".format(name),img_p))
-show_images(images)
- 
+# substitue Folder name with your desired repository
+name = input("Write image name:") # define the name for both the images to save
+print("Original image saved: ", cv2.imwrite(r"Folder\i{0}.png".format(name),image_original))
+print("Rectified image saved: ", cv2.imwrite(r"Folder\i{0}.png".format(name),img_p))
