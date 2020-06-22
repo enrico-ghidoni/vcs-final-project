1b144e78e7ec99960f46ed64921a01f9e482bb56
Enrico Ghidoni <enricoghdn@gmail.com>
2020-06-22 21:16:32 +0200

    First proper working version

--- progetto/detection.py
+++ progetto/detection.py
@@ -48,43 +48,21 @@ def main(video, output_file, onein, debug=False, noshow=False):
             std = np.std(gray)
             gf_size = int(np.ceil(3 * std) // 2 * 2 + 1)
             gk_size = (gf_size, gf_size)
-            gk_size = (13, 13)
+            gk_size = (5, 5)
 
             filtered = cv2.GaussianBlur(gray, gk_size, 0)
             otsu_th, th_img = cv2.threshold(filtered, 180, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
-            # cv2.imshow('thresholded image', th_img)
-            # cv2.waitKey()
 
-            dilated_img = cv2.dilate(th_img, None, iterations=15)
-            # cv2.imshow('dilated image', dilated_img)
-            # cv2.waitKey()
-
-            num_labels, labels_im = cv2.connectedComponentsWithAlgorithm(dilated_img, 8, cv2.CV_32S, cv2.CCL_GRANA)
-            print(num_labels)
-            print(len(labels_im))
-            label_hue = np.uint8(179 * labels_im / np.max(labels_im))
-            blank_ch = 255 * np.ones_like(label_hue)
-            labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
-            labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
-            # cv2.imshow('connected components image', labeled_img)
-            # cv2.waitKey()
-
-            # eroded_img = cv2.dilate(dilated_img, None, iterations=5)
-            # cv2.imshow('eroded image', eroded_img)
-            # cv2.waitKey()
-
-            # edges = cv2.Canny(filtered, 0, 0.5 * otsu_th)
-            # eroded_img = cv2.dilate(edges, None, iterations=4)
-
-            # padded_img = np.pad(eroded_img, (15, 15), mode='constant', constant_values=(1, 1))
+            dilated_img = cv2.dilate(th_img, None, iterations=4)
 
             contours_img = np.copy(rgb_frame)
             contours, hierarchy = cv2.findContours(dilated_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            outer_contour_index = [i for i, h in enumerate(hierarchy[0]) if h[3] == -1]
-            print(outer_contour_index)
-
-            print(f'found {len(contours)} contours')
+            # keep only outer contours
+            outer_contours_index = [i for i, h in enumerate(hierarchy[0]) if h[3] == -1]
+            contours = [contours[i] for i in outer_contours_index]
+            hierarchy = [hierarchy[0][i] for i in outer_contours_index]
+            print(f'outer contours indexes {outer_contours_index}')
 
             contours_poly = [None] * len(contours)
             boundRect = [None] * len(contours)
@@ -93,54 +71,37 @@ def main(video, output_file, onein, debug=False, noshow=False):
                 boundRect[i] = cv2.boundingRect(contours_poly[i])
                 bboxes_json[frame_count].append(boundRect[i])
 
+            # calculate the average area of the bounding boxes to remove the ones below
+            bboxes_area = [box[2] * box[3] for box in boundRect]
+            avg_area = np.mean(bboxes_area)
+            print(f'average box area {avg_area}')
+            boundRect = [box for box in boundRect if (box[2] * box[3]) >= avg_area]
+
             bboxes_img = np.copy(rgb_frame)
-            for i in range(len(contours)):
-                # tengo solo i contours figli dell'outer contour
-                # print(f'contour {i}, hierarchy record {hierarchy[0][i]}')
-                # if hierarchy[0][i][3] in outer_contour_index:
+            for i, box in enumerate(boundRect):
                 color = (rng.randint(0, 256), rng.randint(0, 256), rng.randint(0, 256))
-                color = (255, 0, 0)
-                cv2.drawContours(contours_img, contours_poly, i, color, thickness=3)
-                cv2.rectangle(bboxes_img, (int(boundRect[i][0]), int(boundRect[i][1])), \
-                            (int(boundRect[i][0] + boundRect[i][2]), int(boundRect[i][1] + boundRect[i][3])), color, 3)
+                # cv2.drawContours(contours_img, contours_poly, i, color, thickness=3)
+                cv2.rectangle(bboxes_img, (int(box[0]), int(box[1])), \
+                            (int(box[0] + box[2]), int(box[1] + box[3])), color, 3)
+            if not noshow:
+                if debug:
+                    fig, ax = plt.subplots(2, 2, figsize=(14, 14))
 
-            if debug:
-                fig, ax = plt.subplots(2, 2, figsize=(14, 14))
+                    fig.canvas.set_window_title(f'Frame {frame_count}')
 
-                fig.canvas.set_window_title(f'Frame {frame_count}')
+                    ax[0, 0].imshow(th_img, cmap='gray')
+                    ax[0, 0].set_title(f'threshold')
+                    ax[0, 1].imshow(dilated_img, cmap='gray')
+                    ax[1, 0].set_title(f'labeled image')
+                    ax[1, 1].imshow(bboxes_img)
+                    ax[1, 1].set_title(f'bounding boxes')
 
-                # ax[0, 0].imshow(edges, cmap='gray')
-                # ax[0, 0].set_title('canny')
-                # ax[0, 1].imshow(filtered, cmap='gray')
-                # ax[0, 1].set_title('blurred image')
-                # ax[0, 2].imshow(eroded_img, cmap='gray')
-                # ax[0, 2].set_title('eroded img')
-                # ax[1, 0].imshow(th_img, cmap='gray')
-                # ax[1, 0].set_title('otsu thresholding')
-                # ax[1, 1].imshow(contours_img)
-                # ax[1, 1].set_title('contours')
-                # ax[1, 2].imshow(bboxes_img)
-                # ax[1, 2].set_title('bounding boxes')
-
-                ax[0, 0].imshow(frame, cmap='gray')
-                ax[0, 0].set_title(f'frame')
-                ax[0, 1].imshow(dilated_img, cmap='gray')
-                ax[0, 1].set_title(f'dilated image')
-                ax[1, 0].imshow(labeled_img)
-                ax[1, 0].set_title(f'labeled image')
-                ax[1, 1].imshow(bboxes_img)
-                ax[1, 1].set_title(f'bounding boxes')
-
-                plt.tight_layout()
-
-                # plt.imshow(rgb_frame)
-                # plt.gcf().canvas.mpl_connect('key_press_event', close)
-
-                plt.show()
-            else:
-                out = cv2.cvtColor(bboxes_img, cv2.COLOR_RGB2BGR)
-                cv2.imshow(f'Output', out)
-                cv2.waitKey(75)
+                    plt.tight_layout()
+                    plt.show()
+                else:
+                    out = cv2.cvtColor(bboxes_img, cv2.COLOR_RGB2BGR)
+                    cv2.imshow(f'Output', out)
+                    cv2.waitKey(75)
         if not ret:
             break
 
