03723b524621ff5181c4b18eb4c41a21ab2b1b1d
Tommaso Miana <215835@studenti.unimore.it>
2020-06-25 23:10:06 +0200

    delete descriptors file, add the argparse and fixed some bugs

--- retrieval/image_retrieval.py
+++ retrieval/image_retrieval.py
@@ -16,6 +16,14 @@ BF = cv2.BFMatcher(NORM, crossCheck=True)
 ORB = cv2.ORB_create(500, 1.25)
 
 
+def check_paths(source, findings):
+    if not os.path.exists(source):
+        print('no source of images')
+        exit(1)
+    if args.save_findings:
+        if not os.path.exists(findings):
+            os.mkdir(findings)
+
 class Image:
     def __init__(self, filename, image, descriptors, keypoints):
         self.filename = filename
@@ -37,64 +45,6 @@ class Image:
         }
 
 
-def compute_kp_descr(im, orb):
-    # find keypoints and descriptors from an image given a detector
-    gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    keypoints, descriptors = orb.detectAndCompute(gray_im, None)
-    return keypoints, descriptors
-
-
-def get_paintings(orb, db):
-    """
-    compute the descriptors of all the image of the db with the orb detector
-    :param orb: orb detector
-    :return: list of Image objects
-    """
-    paintings = []
-    with open(PAINTINGS_CSV, 'r') as f:
-        f_reader = csv.DictReader(f)
-        for row in f_reader:
-            im = cv2.imread(f"{db}/{row['Image']}")
-            kp, descr = compute_kp_descr(im, orb)
-            image = Image(filename=row['Image'], image=im, descriptors=descr, keypoints=kp)
-            image.title = row['Title']
-            image.author = row['Author']
-            image.room = row['Room']
-            paintings.append(image)
-    return paintings
-
-
-def nearest_neighbors(query_image, reference_paintings, n_paintings):
-    """
-    Given a list of descriptors <reference_descriptors>, find the <match_num>
-    images that have the most similar descriptors to the <image_descriptors>.
-
-    :param query_image: the image we want to retrieve
-    :param reference_paintings: array of Images representing the paintings of the db
-    :param n_paintings: number of retrieved paintings we want to return
-    :return: array of Image objects. They are sorted from the most to the less
-             similar to the <image_descriptors>
-    """
-
-    paintings = []
-    # find best matches for each reference image
-    for image in reference_paintings:
-        # brute force match
-        matches = BF.match(image.descriptors, query_image.descriptors)
-        # save the sorted matches matches
-        image.matches = sorted(matches, key = lambda x: x.distance)
-        paintings.append(image)
-    # compute for each set of matches the avg 'divergence' to the query one
-    for p in paintings:
-        p.distance = np.mean([match.distance for match in p.matches])
-    sorted_paintings = sorted(paintings, key=lambda x: x.distance)
-    # update best matches on query image
-    matches = BF.match(query_image.descriptors, sorted_paintings[0].descriptors)
-    query_image.matches = sorted(matches, key=lambda x: x.distance)
-    # return a list of paintings sorted by distance from the query one
-    return sorted_paintings[0:n_paintings]
-
-
 def draw_matches(query_image, images_matched, num_kp_matched, accurate):
     """
     Plot both the most similar image found in the db with the corresponding
@@ -105,7 +55,8 @@ def draw_matches(query_image, images_matched, num_kp_matched, accurate):
     """
 
     # Show top num_kp_matched matches
-    title_obj = plt.title(f'Best match: {images_matched[0].title}')
+    correct = 'Correct Match' if accurate else 'Wrong Match'
+    title_obj = plt.title(f'Best match: {images_matched[0].title} -> {correct}')
     plt.setp(title_obj, color='g') if accurate else plt.setp(title_obj, color='r')
     print(f'Best match: {images_matched[0].title}, distance: {images_matched[0].distance}')
     print(f'accurate: {accurate}')
@@ -129,23 +80,6 @@ def draw_matches(query_image, images_matched, num_kp_matched, accurate):
         plt.imshow(images_matched[i].image)
     plt.show()
 
-
-def check_paths(source, findings):
-    if not os.path.exists(source):
-        print('no source of images')
-        exit(1)
-    if args.save_findings:
-        if not os.path.exists(findings):
-            os.mkdir(findings)
-
-
-def is_accurate(paintings):
-    distances = [p.distance for p in paintings[:10]]
-    var = max(distances) - min(distances)
-    if var < 6: # empirical parameter
-        return False
-    else:
-        return True
 
 
 def arg_parse():
@@ -172,35 +106,122 @@ def arg_parse():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    args = arg_parse()
-    check_paths(args.source, args.findings)
-    # those parameters are optimal for this purpose
-    print('--- getting descriptors of each painting in the db... ---')
-    start = time.time()
-    paintings = get_paintings(ORB, args.paintings)
-    end = time.time()
-    print(f'operation took: {end - start}s')
-    for filename in os.listdir(args.source):
+class Retrieval:
+    def __init__(self, paintings_db, findings_dir):
+        orb = cv2.ORB_create(500, 1.25)
+        self.paintings = self.__get_paintings__(orb, paintings_db)
+        self.findings_dir = findings_dir
+
+    def __compute_kp_descr__(self, im, orb):
+        # find keypoints and descriptors from an image given a detector
+        gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        keypoints, descriptors = orb.detectAndCompute(gray_im, None)
+        return keypoints, descriptors
+
+    def __get_paintings__(self, orb, db):
+        """
+        compute the descriptors of all the image of the db with the orb detector
+        :param orb: orb detector
+        :return: list of Image objects
+        """
+        paintings = []
+        with open(PAINTINGS_CSV, 'r') as f:
+            f_reader = csv.DictReader(f)
+            for row in f_reader:
+                im = cv2.imread(f"{db}/{row['Image']}")
+                kp, descr = self.__compute_kp_descr__(im, orb)
+                image = Image(filename=row['Image'], image=im, descriptors=descr, keypoints=kp)
+                image.title = row['Title']
+                image.author = row['Author']
+                image.room = row['Room']
+                paintings.append(image)
+        return paintings
+
+    def __nearest_neighbors__(self, query_image, reference_paintings, n_paintings):
+        """
+        Given a list of descriptors <reference_descriptors>, find the <match_num>
+        images that have the most similar descriptors to the <image_descriptors>.
+
+        :param query_image: the image we want to retrieve
+        :param reference_paintings: array of Images representing the paintings of the db
+        :param n_paintings: number of retrieved paintings we want to return
+        :return: array of Image objects. They are sorted from the most to the less
+                 similar to the <image_descriptors>
+        """
+
+        paintings = []
+        # find best matches for each reference image
+        for image in reference_paintings:
+            # brute force match
+            matches = BF.match(image.descriptors, query_image.descriptors)
+            # save the sorted matches matches
+            image.matches = sorted(matches, key=lambda x: x.distance)
+            paintings.append(image)
+        # compute for each set of matches the avg 'divergence' to the query one
+        for p in paintings:
+            p.distance = np.mean([match.distance for match in p.matches])
+        sorted_paintings = sorted(paintings, key=lambda x: x.distance)
+        # update best matches on query image
+        matches = BF.match(query_image.descriptors, sorted_paintings[0].descriptors)
+        query_image.matches = sorted(matches, key=lambda x: x.distance)
+        # return a list of paintings sorted by distance from the query one
+        return sorted_paintings[0:n_paintings]
+
+    def __is_accurate__(self, paintings):
+        distances = [p.distance for p in paintings[:10]]
+        var = distances[9] - distances[0]
+        if var < 6:  # empirical parameter
+            return False
+        else:
+            return True
+
+    def retrieve_image(self, image, n_matches=10, save_findings=True, show=False):
         if filename.endswith('png'):
-            print(f'--- retrieve the painting {filename}... ---')
+
             start = time.time()
-            im = cv2.imread(f'{args.source}/{filename}')
-            kp, descr = compute_kp_descr(im, ORB)
-            query_image = Image('query_image', im, descr, kp)
-            # findings is an array of Images
-            findings = nearest_neighbors(query_image, paintings, args.n_matches)
-            accurate = is_accurate(findings)
+
+            # compute the descr and the kp of the image and save it as Image class
+            kp, descr = self.__compute_kp_descr__(image, ORB)
+            query_image = Image('query_image', image, descr, kp)
+
+            print(f'--- retrieve the painting {query_image.filename}... ---')
+
+            # do the retrieval
+            findings = self.__nearest_neighbors__(query_image, self.paintings, n_matches)
+            accurate = self.__is_accurate__(findings)
             end = time.time()
             print(f'operation took: {end - start}s')
-            if args.save_findings:
+            if save_findings:
                 finding = {}
                 finding['frame'] = filename
                 json_findings = [f.get_json() for f in findings]
                 finding['paintings'] = json_findings
                 finding['accurate'] = accurate
-                with open(f'{args.findings}/findings.json', 'w+') as outfile:
+                with open(f'{self.findings_dir}/findings.json', 'w+') as outfile:
                     json.dump(finding, outfile)
-            if args.show:
-                draw_matches(query_image, findings, args.n_matches, accurate)
+            if show:
+                draw_matches(query_image, findings, n_matches, accurate)
+        else:
+            print('format error')
+            return None
+        return None
+
+
+if __name__ == '__main__':
+
+    args = arg_parse()
+    check_paths(args.source, args.findings)
+
+    # those parameters are optimal for this purpose
+    print('--- getting descriptors of each painting in the db... ---')
+    start = time.time()
+
+    ret = Retrieval(args.paintings, args.findings)
+
+    end = time.time()
+    print(f'operation took: {end - start}s')
+
+    for filename in os.listdir(args.source):
+        im = cv2.imread(f'{args.source}/{filename}')
+        ret.retrieve_image(im, show=True)
 
