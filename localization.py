f8c5eb734991cd08694f40a7a3135280ebf6b93e
Enrico Ghidoni <enricoghdn@gmail.com>
2020-06-29 22:27:40 +0200

    Fix typos

--- localization.py
+++ localization.py
@@ -5,14 +5,13 @@ import sys
 import csv
 
 
-
 class PeopleLocalization(object):
 
     def __init__(self, path_csv):
         self.room = 0
         self.path_data = path_csv
 
-    def openCSV(self, path):    
+    def openCSV(self, path):
         try:
             # CSV file opening
             f = open(path,)
@@ -25,9 +24,9 @@ class PeopleLocalization(object):
     def drawBbox(self, frame_num, bbox, img, room):
         coordinates = bbox[frame_num]
         for coordinate in coordinates:
-                x,y,w,h = coordinate
-                cv2.rectangle(img, (x, y), (x+w, y+h), (255,100,100), 3)
-                cv2.putText(img, f"Room {room}",(x - 10, y - 10),cv2.FONT_ITALIC,1,(255,100,100),2)
+            x,y,w,h = coordinate
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255,100,100), 3)
+            cv2.putText(img, f"Room {room}",(x - 10, y - 10),cv2.FONT_ITALIC,1,(255,100,100),2)
 
     def assign_rooms(self, frame_num, image_name):
 
@@ -37,10 +36,10 @@ class PeopleLocalization(object):
                 if image[0] == image_name:
                     room = image[2]
 
-            return int(room)    
+            return int(room)
         except Exception:
             return 0
-        
+
     def people_localization(self, frame, frame_num, bbox, image_name):
 
         room = self.assign_rooms(frame_num,image_name)
